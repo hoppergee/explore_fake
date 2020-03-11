44643488dff75e0a17c034da56308d74ac234670
吉翔 <jx4554@gmail.com>
2020-03-11 23:32:13 +0800

    Initial commit

---
+++ lib/explore_fake.rb
@@ -0,0 +1,6 @@
+require "explore_fake/version"
+
+module ExploreFake
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/explore_fake/version.rb
@@ -0,0 +1,3 @@
+module ExploreFake
+  VERSION = "0.1.0"
+end
